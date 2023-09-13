1cf512988094f82571fdebe458d698646130a680
Shahariar Sohan <Sohanshahariar4@gmail.com>
2023-09-13 11:56:36 +0600

    my ist commit for movie maker team

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        {" "}
+        <h1 className=" text-center text-3xl font-bold">MOVIE MAKING</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
